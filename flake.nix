9eb96976c97a3ca73cab527139d849e716c2427a
NoRePercussions <tucker@tuckershea.com>
2024-12-15 15:48:24 -0500

    fix: set better description

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
-  description = "My personal NUR repository";
+  description = "tuckershea's personal NUR repository";
+
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
   outputs = { self, nixpkgs }:
     let
       systems = [
